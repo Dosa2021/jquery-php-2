90c664abcef5aa53a3b336cef8642a5969c88b68
mel_sugawara <mel_sugawara@mel-mac-kinoshita.local>
2026-06-05 08:44:47 +0900

    一覧取得API作成

--- php/api/index.php
+++ php/api/index.php
@@ -1,10 +1,21 @@
 <?php
-header('Content-Type: application/json');
+header('Content-Type: application/json; charset=UTF-8');
 
+$dsn = 'mysql:host=mysql;dbname=test;charset=utf8';
+$user = 'root';
+$password = 'root_password';
 
-// JSONデータを返す
-$response = [
-    'message' => 'Docker環境のPHPからデータが送信されました！'
-];
+try {
+    $dbh = new PDO($dsn, $user, $password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-echo json_encode($response);
+    $stmt = $dbh->prepare('SELECT * FROM test_user');
+    $stmt->execute();
+
+    $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    echo json_encode($users, JSON_UNESCAPED_UNICODE);
+} catch (PDOException $e) {
+    http_response_code(500);
+    echo json_encode(['error' => 'Database error'], JSON_UNESCAPED_UNICODE);
+}

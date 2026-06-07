7e8774ad7fb58590c536ce684f4f92d962c60dae
mel_sugawara <mel_sugawara@mel-mac-kinoshita.local>
2026-06-07 18:52:48 +0900

    一旦コメントアウト

--- php/views/home.php
+++ php/views/home.php
@@ -1,10 +1,10 @@
 <?php
-require_once $_SERVER['DOCUMENT_ROOT'].'/models/TestModel.php';
+// require_once $_SERVER['DOCUMENT_ROOT'].'/models/TestModel.php';
 require_once $_SERVER['DOCUMENT_ROOT'].'/views/layout/header.php';
 
-$userModel = new TestModel();
+// $userModel = new TestModel();
 
-$tests = $userModel->getAllTests();
+// $tests = $userModel->getAllTests();
 
 // var_dump('test-------');
 // var_dump($tests);
